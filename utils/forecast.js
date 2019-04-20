abfc3a32b973729f87c28fcf8208914df12ee2a6
grvcodes <45913521+grvcodes@users.noreply.github.com>
2019-04-20 15:12:22 +0530

    changed units for forecast data

--- utils/forecast.js
+++ utils/forecast.js
@@ -2,7 +2,7 @@ const request = require('request');
 require('dotenv').config();
 
 const forecast = (geocode,callback)=>{
-    const url = `https://api.darksky.net/forecast/${process.env.FORECAST}/${geocode.latitude},${geocode.longitude}`;
+    const url = `https://api.darksky.net/forecast/${process.env.FORECAST}/${geocode.latitude},${geocode.longitude}?units=auto`;
     request({url :url,json: true },(err,res)=>{
         if(err){
             console.log(error);
